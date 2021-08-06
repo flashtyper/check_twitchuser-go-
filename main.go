8dd35387020c75c34a556e9f70795905ba6ee114
Lukas <83031404+flashtyper@users.noreply.github.com>
2021-08-06 19:52:35 +0200

    improved error checking

--- main.go
+++ main.go
@@ -26,11 +26,11 @@ func main() {
 
         if len(parsedData.Data) > 0 {
                 //User ONLINE - CRITICAL
-                fmt.Printf("CRITICAL - %s is live!\n\nTitle: %s \nGame: %s | viewer=%d \n", parsedData.Data[0].UserName, parsedData.Data[0].Title, parsedData.Data[0].GameName, parsedData.Data[0].ViewerCount)
+                fmt.Printf("CRITICAL - %s ist live!\n\nTitel: %s \nGame: %s | viewer=%d \n", parsedData.Data[0].UserName, parsedData.Data[0].Title, parsedData.Data[0].GameName, parsedData.Data[0].ViewerCount)
                 os.Exit(2)
         } else {
                 //User OFFLINE - OK
-        fmt.Printf("OK - %s is offline. | viewer=0\n", *user)
+        fmt.Printf("OK - %s ist offline. | viewer=0\n", *user)
                 os.Exit(0)
         }
 }
@@ -41,11 +41,15 @@ func rest(user string, clientid string, str_token string) (arr_resp []byte){
         req.Header.Add("client-id", clientid)
         req.Header.Add("Authorization", str_token)
         resp, err := client.Do(req)
+        if err != nil {
+                fmt.Print(err.Error())
+                os.Exit(3)
+        }
         arr_resp, err = ioutil.ReadAll(resp.Body)
         if err != nil {
                 fmt.Print(err.Error())
+                os.Exit(3)
         }
-
         if (resp.StatusCode != 200) {
                 s := string(arr_resp)
                 e := strings.ReplaceAll(s, "\n", "")
